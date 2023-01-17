411753bb25e49a4ad91f87f3efeb2c3f5a0806f6
Paulo Rogerio de Pinho <101976615+dePinhoPaulo@users.noreply.github.com>
2023-01-17 16:44:42 -0300

    Add files via upload

---
+++ JavaScript/Fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1515;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nomes genericos e siglas
+
+let valor = ''
+let numero = 1
+let pqp = false // produto quimico perigoso
