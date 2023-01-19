a43a034608362550e449a2238bf4fff01caad445
Paulo Rogerio de Pinho <101976615+dePinhoPaulo@users.noreply.github.com>
2023-01-19 10:12:08 -0300

    Add files via upload

---
+++ JavaScript/Estruturas-de-Controle/ifElse.js
@@ -0,0 +1,11 @@
+function imprimirResultado (num){
+    if(num > 7){
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+imprimirResultado('Epa!')//cuidado!
