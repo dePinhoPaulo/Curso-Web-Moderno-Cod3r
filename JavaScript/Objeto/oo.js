6c926c5b7b46796458e99c5ae7258e8b07f07f98
Paulo Rogerio de Pinho <101976615+dePinhoPaulo@users.noreply.github.com>
2023-01-26 11:46:51 -0300

    Add files via upload

---
+++ JavaScript/Objeto/oo.js
@@ -0,0 +1,22 @@
+//CODIGO NAO EXECUTAVEL!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//oo
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento()
+
+//Principios importantes:
+//1. abstração
+//2. encapsulamento
+//3. herança (prototype)
+//4. polimorfismo 
