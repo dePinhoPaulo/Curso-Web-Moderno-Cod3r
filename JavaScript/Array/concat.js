0582f8462511d6390bf8d692e440d237665734e9
Paulo Rogerio de Pinho <101976615+dePinhoPaulo@users.noreply.github.com>
2023-01-27 13:27:09 -0300

    Add files via upload

---
+++ JavaScript/Array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['valeska', 'Cibalena']
+const filhos = ['Joao', 'ze']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos)
+
+console.log([].concat([1,2], [3,4], 5, [[6,7]]))
